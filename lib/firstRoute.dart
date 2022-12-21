f314364eedfd3cf0af3ad1335ea4671cd1ffcb73
AlexDor8 <st2022273@365.stucom.com>
2022-12-21 16:51:23 +0100

    ahora sí, practica acabada

--- lib/firstRoute.dart
+++ lib/firstRoute.dart
@@ -68,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
            SizedBox(
             width: 300,
             child: TextField(
+              obscureText: true,
               decoration: const InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
